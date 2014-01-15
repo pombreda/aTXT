a48a960db8a58f51210d7d23188a4c309cb1142a
Jonathan Prieto <prieto.jona@gmail.com>
2014-01-15 10:41:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-# setup file
+from distutils.core import setup
+    setup(
+        name = 'aTXT',
+        packages = ['aTXT'], # this must be the same as the name above
+        version = '0.1',
+        description = 'docx, pdf, doc(windows) to txt format file. Extraction`s tool.',
+        author = 'Jonathan Prieto',
+        author_email = 'prieto.jona@gmail.com',
+        url = 'https://github.com/d555/aTXT',   # use the URL to the github repo
+        download_url = 'https://github.com/d555/aTXT/0.1', # I'll explain this in a second
+        keywords = ['doc', 'docx', 'pdf', 'docx2txt', 'pdf2txt', 'doc2txt', 'txt', 'extraction', 'office'], # arbitrary keywords
+        classifiers = [],
+    )
